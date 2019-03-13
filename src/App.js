70491ec230180ef74f48d8904b3290e7147fc350
Sohad Dader <sohad@leannode.com>
2019-03-13 20:36:29 +0200

    change document title

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
 
 export default class App extends Component {
   constructor(props) {
@@ -14,6 +15,14 @@ export default class App extends Component {
     };
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleSurenameChange = this.handleSurenameChange.bind(this);
+  }
+
+  componentDidMount() {
+    document.title = this.state.name + ' ' + this.state.surename;
+  }
+
+  componentDidUpdate() {
+    document.title = this.state.name + ' ' + this.state.surename;
   }
 
   handleNameChange(e) {
@@ -30,9 +39,13 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{ backgroundColor: '#E9D7EE' }}>
         <AppBar
-          style={{ backgroundColor: 'rgb(109, 30, 124)', color: 'white' }}
+          style={{
+            flex: 1,
+            backgroundColor: 'rgb(109, 30, 124)',
+            color: 'white'
+          }}
           position="static"
           color="default"
         >
@@ -40,33 +53,24 @@ export default class App extends Component {
             <Typography
               variant="h6"
               color="inherit"
-              style={{ flex: 1, justifyContent: 'center' }}
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                height: '100%'
+              }}
             >
               Hooks with Reactjs
             </Typography>
           </Toolbar>
         </AppBar>
-        <form
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            backgroundColor: 'rgba(230, 205, 235, 0.56)'
-          }}
-          noValidate
-          autoComplete="off"
-        >
+        <form noValidate autoComplete="off">
           <TextField
             label="Name"
-            style={{
-              marginLeft: 30,
-              marginRight: 30
-            }}
             value={this.state.name}
             onChange={this.handleNameChange}
             margin="normal"
             variant="outlined"
           />
-
           <TextField
             label="Sure Name"
             style={{
@@ -79,6 +83,24 @@ export default class App extends Component {
             variant="outlined"
           />
         </form>
+        <div
+          style={{
+            flex: 1,
+            width: '40%',
+            marginLeft: '28%',
+            height: 300
+          }}
+        >
+          <br />
+          <Paper>
+            <Typography variant="h7" component="h3">
+              Your full name that you insert it :
+            </Typography>
+            <Typography variant="h3" style={{ color: '#6C237A' }}>
+              {this.state.name} {this.state.surename}
+            </Typography>
+          </Paper>
+        </div>
       </div>
     );
   }
