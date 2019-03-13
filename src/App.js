aa6afb0b1caaa61c54628788a756e7915996a208
Sohad Dader <sohad@leannode.com>
2019-03-13 14:43:12 +0200

    add useEffects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
 
 export default function App(props) {
   const [name, setName] = useState('Sohad');
@@ -17,10 +18,18 @@ export default function App(props) {
     setSurename(e.target.value);
   }
 
+  useEffect(() => {
+    document.title = name + ' ' + surname;
+  });
+
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundColor: '#E9D7EE' }}>
       <AppBar
-        style={{ backgroundColor: 'rgb(109, 30, 124)', color: 'white' }}
+        style={{
+          flex: 1,
+          backgroundColor: 'rgb(109, 30, 124)',
+          color: 'white'
+        }}
         position="static"
         color="default"
       >
@@ -28,27 +37,19 @@ export default function App(props) {
           <Typography
             variant="h6"
             color="inherit"
-            style={{ flex: 1, justifyContent: 'center' }}
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              height: '100%'
+            }}
           >
             Hooks with Reactjs
           </Typography>
         </Toolbar>
       </AppBar>
-      <form
-        style={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          backgroundColor: 'rgba(230, 205, 235, 0.56)'
-        }}
-        noValidate
-        autoComplete="off"
-      >
+      <form noValidate autoComplete="off">
         <TextField
           label="Name"
-          style={{
-            marginLeft: 30,
-            marginRight: 30
-          }}
           value={name}
           onChange={handleNameChange}
           margin="normal"
@@ -67,6 +68,24 @@ export default function App(props) {
           variant="outlined"
         />
       </form>
+      <div
+        style={{
+          flex: 1,
+          width: '40%',
+          marginLeft: '28%',
+          height: 300
+        }}
+      >
+        <br />
+        <Paper>
+          <Typography variant="h7" component="h3">
+            Your full name that you insert it :
+          </Typography>
+          <Typography variant="h3" style={{ color: '#6C237A' }}>
+            {name} {surname}
+          </Typography>
+        </Paper>
+      </div>
     </div>
   );
 }
